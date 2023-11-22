41f3ac28b0f0f0198e15447ade6d456aef0f2435
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-11-22 11:32:36 +0200

    add tracker app and create models.py and admin.py and made migrations

---
+++ tracker/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, MealPlan, NutritionTracker, Food
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("weight", "height")
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("weight", "height", "age", "fitness_goals")}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "weight",
+                        "height",
+                        "age",
+                        "fitness_goals"
+                    )
+                },
+            ),
+        )
+    )
+
+@admin.register(MealPlan)
+class MealPlanAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+    list_filter = ("foods",)
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    search_fields = (
+        "name",
+        "protein",
+        "carbs",
+        "fats",
+    )
+    list_filter = ("meal_plans",)
+
+admin.site.register(NutritionTracker)
---
+++ tracker/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=255)
+    calories = models.FloatField()
+    protein = models.FloatField()
+    carbs = models.FloatField()
+    fats = models.FloatField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    age = models.IntegerField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    fitness_goals = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class MealPlan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="meal_plans")
+    foods = models.ManyToManyField(Food, related_name="meal_plans")
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class NutritionTracker(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="nutrition_trackers")
+    food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name="nutrition_trackers")
+    date = models.DateTimeField(auto_now_add=True)
+    serving_size = models.IntegerField()
+    meal_type = models.CharField(max_length=255)
+    user_notes = models.TextField(blank=True)
