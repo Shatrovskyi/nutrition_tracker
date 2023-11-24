6088477e470be1fc0a3dfee588124fde269826af
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-11-24 16:12:38 +0200

    improvements

--- nutrition_tracker/settings.py
+++ nutrition_tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG")
+DEBUG = os.environ["DJANGO_DEBUG"]
 
 ALLOWED_HOSTS = ["127.0.0.1", "nutrition-tracker-z8km.onrender.com"]
 
