c68d3060b4bc3d7789354f69a297a70b038f00d4
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-11-24 15:05:38 +0200

    Improved Settings

--- nutrition_tracker/settings.py
+++ nutrition_tracker/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-3-#l8ril$m&^zhmj#j2gz%)__^jsbv-^0gj#kqv!!%1naq@d^i")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
@@ -88,7 +89,6 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
-DATABASE_URL = "postgres://bukexfwu:IA0uFD_hlcBtq8FZvnw06zton54wil78@cornelius.db.elephantsql.com/bukexfwu"
 
 
 # Password validation
@@ -134,7 +134,7 @@ STATIC_ROOT = "staticfiles/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-#STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 INTERNAL_IPS = [
     "127.0.0.1",
